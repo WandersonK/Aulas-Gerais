6d633493819f04e201d63175bd9ac37a1a6ad2ca
WandersonK <wan01dez0013@gmail.com>
2022-03-21 10:06:30 -0300

    Primeiro Comiit

---
+++ Solyd/Introdução ao Python/aula2-entrada-e-saida.py
@@ -0,0 +1,15 @@
+nome = 'Wanderson'
+idade = 25
+altura = 1.7
+tipo_nome = type(nome)
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+
+# print(nome)
+# print(idade)
+# print(altura)
+# print(tipo_nome)
+# print(tipo_idade)
+# print(tipo_altura)
+
+print("Olá, tudo bem?")
